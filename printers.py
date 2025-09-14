08b9bf5ae04ae3ec3acd769c905ef2eb463bdb5c
Valentín Ceballos <108242764+vjceballosj@users.noreply.github.com>
2025-09-13 20:24:24 -0500

    Print

---
+++ printers.py
@@ -0,0 +1,18 @@
+print('Hello, World!') # The text Hello, World! is an argument
+print('Hello',',', 'World','!') # it can take multiple arguments, four arguments have been passed
+print(len('Hello, World!')) # it takes only one argument
+
+# Printing the values stored in the variables
+
+first_name, last_name, country, city, age, is_married, city, skills, person_info = 'Valentín', 'Dev', 'Colombia', 'City', 250, True, 'Java', 'Python', 'ValentoDev'
+
+print('First name:', first_name)
+print('First name length:', len(first_name))
+print('Last name: ', last_name)
+print('Last name length: ', len(last_name))
+print('Country: ', country)
+print('City: ', city)
+print('Age: ', age)
+print('Married: ', is_married)
+print('Skills: ', skills)
+print('Person information: ', person_info)
