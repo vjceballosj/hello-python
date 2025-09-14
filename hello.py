333373084bf794087cbb1cb14e97e11e623cfbab
Valentín Ceballos <108242764+vjceballosj@users.noreply.github.com>
2025-09-13 20:11:56 -0500

    first commit python

---
+++ hello.py
@@ -0,0 +1,9 @@
+# This is the first comment
+# This is the second comment
+# Esto es un comentario, Hola mundo en Python
+"""
+This is multiline comment
+multiline comment takes multiple lines.
+python is easting the world
+"""
+print("Hello, Python")
