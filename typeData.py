048700f763a925d3fda0d53c0fe4ebb74131f4a7
Valentín Ceballos <108242764+vjceballosj@users.noreply.github.com>
2025-09-13 20:12:50 -0500

    TypeData in python

---
+++ typeData.py
@@ -0,0 +1,17 @@
+print(type("Hello Python")) # Tipo 'str'
+print(type(7)) # Tipo 'int'
+print(type(7.7)) # Tipo 'float'
+print(type(True)) # Tipo 'bool'
+print(type([1,2,3,4,5])) # Tipo 'list'
+print(type({'first_name':'Valentín',
+            'last_name':'Ceballos'})) # Tipo 'object' ...
+
+# Checking data types
+print(type(10))          # Int
+print(type(3.14))        # Float
+print(type(1 + 3j))      # Complex number
+print(type('Asabeneh'))  # String
+print(type([1, 2, 3]))   # List
+print(type({'name':'Asabeneh'})) # Dictionary
+print(type({9.8, 3.14, 2.7}))    # Set
+print(type((9.8, 3.14, 2.7)))    # Tuple
