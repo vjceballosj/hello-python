b2bbf08705f40c85a2efdfd69835d7027bca128a
Valentín Ceballos <108242764+vjceballosj@users.noreply.github.com>
2025-09-13 20:24:37 -0500

    variables

---
+++ variable.py
@@ -0,0 +1,40 @@
+"""
+# Variables validas
+firstname
+lastname
+age
+country
+city
+first_name
+last_name
+capital_city
+_if # if we want to use reserved word as a variable
+year_2021
+year2021
+current_year_2021
+birth_year
+num1
+num2
+
+# Variables inválidas
+first-name
+first@name
+first$name
+num-1
+1num
+"""
+
+# Variables in Python
+first_name = 'Valentín'
+last_name = 'Dev'
+country = 'Colombia'
+city = 'City'
+age = 250
+is_married = True
+skills = ['HTML', 'CSS', 'JS', 'React', 'Python']
+person_info = {
+   'firstname':'Valentín',
+   'lastname':'Dev',
+   'country':'Colombia',
+   'city':'City'
+   }
