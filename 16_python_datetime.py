258b576412432cd2f7d5b1af7bea418ae40280a7
Valentín Ceballos <108242764+vjceballosj@users.noreply.github.com>
2025-09-15 15:27:56 -0500

    Create 16_python_datetime.py

---
+++ 16_python_datetime.py
@@ -0,0 +1,38 @@
+#Python datetime
+#Python has got datetime module to handle date and time.
+
+import datetime
+print(dir(datetime))
+['MAXYEAR', 'MINYEAR', '__builtins__', '__cached__', '__doc__', '__file__', '__loader__', '__name__', '__package__', '__spec__', 'date', 'datetime', 'datetime_CAPI', 'sys', 'time', 'timedelta', 'timezone', 'tzinfo']
+
+#With dir or help built-in commands it is possible to know the available functions in a certain module. As you can see, in the datetime module there are many functions, but we will focus on date, datetime, time and timedelta. Let se see them one by one.
+
+#Getting datetime Information
+from datetime import datetime
+now = datetime.now()
+print(now)                      # 2021-07-08 07:34:46.549883
+day = now.day                   # 8
+month = now.month               # 7
+year = now.year                 # 2021
+hour = now.hour                 # 7
+minute = now.minute             # 38
+second = now.second
+timestamp = now.timestamp()
+print(day, month, year, hour, minute)
+print('timestamp', timestamp)
+print(f'{day}/{month}/{year}, {hour}:{minute}')  # 8/7/2021, 7:38
+
+#Timestamp or Unix timestamp is the number of seconds elapsed from 1st of January 1970 UTC.
+
+#Formatting Date Output Using strftime
+from datetime import datetime
+new_year = datetime(2020, 1, 1)
+print(new_year)      # 2020-01-01 00:00:00
+day = new_year.day
+month = new_year.month
+year = new_year.year
+hour = new_year.hour
+minute = new_year.minute
+second = new_year.second
+print(day, month, year, hour, minute) #1 1 2020 0 0
+print(f'{day}/{month}/{year}, {hour}:{minute}')  # 1/1/2020, 0:0
