90bec8f992d2825eef432b0c275af66d30318a11
Valentín Ceballos <108242764+vjceballosj@users.noreply.github.com>
2025-09-15 10:20:47 -0500

    Create 07_sets.py

---
+++ 07_sets.py
@@ -0,0 +1,19 @@
+# syntax list
+lst = list()
+empty_list = list() # this is an empty list, no item in the list
+print(len(empty_list)) # 0
+#Using square brackets, []
+lst = []
+empty_list = [] # this is an empty list, no item in the list
+print(len(empty_list)) # 0
+
+# syntax tuple
+empty_tuple = ()
+# or using the tuple constructor
+empty_tuple = tuple()
+
+# syntax set
+st = set()
+
+#Creating a set with initial items
+st = {'item1', 'item2', 'item3', 'item4'}
