d64f72ec8ecaae44625e639f1878d03118396677
Valentín Ceballos <108242764+vjceballosj@users.noreply.github.com>
2025-09-15 15:10:13 -0500

    Create 14_higher_order_functions.py

---
+++ 14_higher_order_functions.py
@@ -0,0 +1,51 @@
+"""
+Higher Order Functions
+In Python functions are treated as first class citizens, allowing you to perform the following operations on functions:
+
+A function can take one or more functions as parameters
+A function can be returned as a result of another function
+A function can be modified
+A function can be assigned to a variable
+In this section, we will cover:
+
+Handling functions as parameters
+Returning functions as return value from another functions
+Using Python closures and decorators
+Function as a Parameter
+"""
+def sum_numbers(nums):  # normal function
+    return sum(nums)    # a sad function abusing the built-in sum function :<
+
+def higher_order_function(f, lst):  # function as a parameter
+    summation = f(lst)
+    return summation
+result = higher_order_function(sum_numbers, [1, 2, 3, 4, 5])
+print(result)       # 15
+
+#Function as a Return Value
+def square(x):          # a square function
+    return x ** 2
+
+def cube(x):            # a cube function
+    return x ** 3
+
+def absolute(x):        # an absolute value function
+    if x >= 0:
+        return x
+    else:
+        return -(x)
+
+def higher_order_function(type): # a higher order function returning a function
+    if type == 'square':
+        return square
+    elif type == 'cube':
+        return cube
+    elif type == 'absolute':
+        return absolute
+
+result = higher_order_function('square')
+print(result(3))       # 9
+result = higher_order_function('cube')
+print(result(3))       # 27
+result = higher_order_function('absolute')
+print(result(-3))      # 3
